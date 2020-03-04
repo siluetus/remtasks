ab2524b6f30265b498f196e3050988a50b5304db
BingoUser <BingoUser@192.168.0.12>
2020-03-04 22:19:57 +0300

    add: sample jar and sample jar loader

---
+++ src/main/java/taskqueue/runjar/Test.java
@@ -0,0 +1,33 @@
+package taskqueue.runjar;
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+
+public class Test {
+
+	public static void main(String[] args) {
+		try {
+		    File jarfile = new File("Run-Task1.jar");
+		    JarFile jar = new JarFile(jarfile);
+		    Manifest manifest = jar.getManifest();
+		    Attributes attrs = manifest.getMainAttributes();
+		    String mainClassName = attrs.getValue("Main-Class");
+		    
+		    URL url = new URL("file", null, jarfile.getAbsolutePath());
+		    ClassLoader cl = new URLClassLoader(new URL[] {url});
+		    Class mainClass = cl.loadClass(mainClassName);
+		    Method mainMethod = mainClass.getMethod("main", new Class[] {String[].class});
+		    String[] args2 = new String[0];
+		    //System.arraycopy(args, 1, args2, 0, args.length - 1);
+		    mainMethod.invoke(mainClass, new Object[] {args2});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
